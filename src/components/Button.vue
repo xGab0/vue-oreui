56704ece5cd7e61053de4aba424c031323569aef
xGab0 <aricogabriele2001@gmail.com>
2024-02-16 11:32:05 +0100

    backup

---
+++ src/components/Button.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="button-container">
+    <p>button text</p>
+  </div>
+  <div class="button-height"/>
+</template>
+
+<style scoped>
+.button-container {
+  width: 400px;
+  height: 70px;
+
+  background-color: #3b8526;
+
+  border-color: none;
+  border-style: solid;
+  border-width: 4px 4px 0px 4px;
+
+  box-shadow: inset 0px 0px 0px 8px #3ca220;
+  /*box-shadow:
+    inset 0 4px #6bc349,
+    inset 0 -4px #2a631c,
+    0 4px rgba(0, 0, 0, .25);*/
+}
+.button-container:hover {
+  cursor: move;
+}
+.button-container p {
+  text-align: center;
+
+  color: white;
+}
+.button-height {
+  width: 400px;
+  height: 20px;
+
+  background-color: #3b8526;
+
+  border-color: none;
+  border-style: solid;
+  border-width: 0px 4px 4px 4px;
+}
+.button-height:hover {
+  height: 0px;
+
+  background-color: red;
+}
+/*.parent:hover .child, .parent.hover .child { display: block; }*/
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import Button from '../components/Button.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!--TheWelcome /-->
+    <Button />
   </main>
 </template>
